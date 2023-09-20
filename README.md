5fc3e50b2d76a31db249f639cd95d6dd89d11dfe
kfilios <k.filios@gmail.com>
2023-09-20 08:39:38 +0300

    Basic application setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cat-nerds-demo
-A demo for cat nerds.
+## Available Scripts
+
+### `npm start`
+
+Runs in development mode. [http://localhost:3000](http://localhost:3000).
+
+### `npm test`
+
+Starts Jest.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// Logo downloaded from: https://icon-icons.com/icon/cat/138789
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Hello to cat nerds!</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
